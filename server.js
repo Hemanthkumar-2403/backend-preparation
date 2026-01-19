18a47721537256437715f10ae09db72455414f29
Hemanthkumar-2403 <khemanthkumarcse@gmail.com>
2026-01-19 22:20:26 +0530

    feat: add GET and POST routes

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ const express = require("express")
 const app=express()
 require("dotenv").config()
 
+app.use(express.json())
 app.get("/health" ,(req,res)=>{
     res.send("Server is running")
 });
+
+app.post("/signup",(req,res)=>{
+    const data =req.body;
+    res.json({
+        message :"Signup sucess",
+        data :data,
+    })
+})
 
 const port =process.env.port || 4000
 
